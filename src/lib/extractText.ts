262a3529cce58181d1e0cafb248e836846d3c2eb
akashsahoo-1 <akashsahoo1420059@gmail.com>
2026-03-20 00:27:46 +0530

    remove pdfjs to fix build

--- src/lib/extractText.ts
+++ src/lib/extractText.ts
@@ -1,9 +1,5 @@
-import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.mjs'
 import officeParser from 'officeparser'
 import Tesseract from 'tesseract.js'
-
-// Need this to avoid worker errors in node environments or setup the fake worker
-pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/legacy/build/pdf.worker.mjs'
 
 export async function extractText(file: File): Promise<string> {
   const fileType = file.type
@@ -11,7 +7,7 @@ export async function extractText(file: File): Promise<string> {
   const buffer = Buffer.from(arrayBuffer)
 
   if (fileType === 'application/pdf') {
-    return await extractPdfText(arrayBuffer)
+    throw new Error('PDF parsing is temporarily disabled.')
   } else if (
     fileType === 'application/vnd.openxmlformats-officedocument.presentationml.presentation' ||
     fileType === 'application/vnd.ms-powerpoint' ||
@@ -26,25 +22,6 @@ export async function extractText(file: File): Promise<string> {
   }
 }
 
-async function extractPdfText(buffer: ArrayBuffer): Promise<string> {
-  const data = new Uint8Array(buffer)
-  const loadingTask = pdfjsLib.getDocument({
-    data,
-    useSystemFonts: true,
-    standardFontDataUrl: `node_modules/pdfjs-dist/standard_fonts/`
-  })
-  
-  const pdf = await loadingTask.promise
-  let text = ''
-  
-  for (let i = 1; i <= pdf.numPages; i++) {
-    const page = await pdf.getPage(i)
-    const content = await page.getTextContent()
-    text += content.items.map((item: any) => item.str).join(' ') + '\n'
-  }
-  
-  return text
-}
 
 async function extractPptText(buffer: Buffer): Promise<string> {
   const result = await officeParser.parseOffice(buffer)
